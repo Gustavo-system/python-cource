3279b15e7bbb70d1c17c3cb02c8c8f2276aaf76c
GustavoSolar <f170015@gmail.com>
2023-02-20 23:35:25 -0600

    se agregan nuevo conceptos de rutas en fast

--- curso_python_platzi/fastApi/01-fastApi-Hello-word/main.py
+++ curso_python_platzi/fastApi/01-fastApi-Hello-word/main.py
@@ -7,3 +7,15 @@ async def home():
     return {"saludo" : "Hello word"}
     # agregamos la siguente linea en la terminal de comandos para levantar el servidor
     # uvicorn <nombre_archivo> : <objeto_app> --reload
+
+
+@app.get('/saludo/{nombre}') # en este caso siempre se solicita de forma oblugatoria la variable por path
+async def home(nombre:str):
+    return {"saludo" : (f'Hola {nombre}')}
+    # /saludo/gustavo
+
+
+@app.get('/despedida/{nombre}') # se sigue solicitando un parametro obligatorio
+async def home(nombre:str, apellido: str = "", edad: int = 0): # en este caso las querys son opcionales
+    return {"saludo" : (f'Hola {nombre} {apellido} tiene {edad}')}
+    # /despedida/gustavo?apellido=feliz&edad=24
--- curso_python_platzi/fastApi/README.md
+++ curso_python_platzi/fastApi/README.md
@@ -1,3 +1,9 @@
 # FastApi
 
 Se debe crear un entorno de desarrollo
+
+Agregamos la siguente linea en la terminal de comandos para levantar el servidor
+
+```
+uvicorn <nombre_archivo> : <objeto_app> --reload
+```
