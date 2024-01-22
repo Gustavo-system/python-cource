7bf8e06995fd33df3151460445c0e415d8460d35
GUSTAVO SOLAR GAONA <gustavo.solar@elektra.com.mx>
2024-01-22 14:14:23 -0600

    se agrega sobre escritura de usuarios para tener mayor control sobre sus modelos

--- practicas/django/django_rest_framework/primera_api/blog/users/admin.py
+++ practicas/django/django_rest_framework/primera_api/blog/users/admin.py
@@ -5,10 +5,12 @@ from users.models import User
 # Register your models here.
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-	pass
+	list_display = ['id', 'username', 'email', 'is_staff']
+	
 	"""Agregamos las modificaciones para la vista el usuario en el admin web"""
-	# fieldsets = (
-	# 	(None, {'fields': ('username', 'password')}),
-	# 	('Datos personales', {'fields': ('first_name', 'last_name', 'email')}),
-	# 	('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')})
-	# )
+	fieldsets = (
+		(None, {'fields': ('username', 'password')}),
+		('Datos personales', {'fields': ('first_name', 'last_name', 'email')}),
+		('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+		('Redes Solciales', {'fields': ('pagina_web', 'facebook')})
+	)
--- practicas/django/django_rest_framework/primera_api/blog/users/models.py
+++ practicas/django/django_rest_framework/primera_api/blog/users/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+# se declara el modelo vacio ya que se esta heredando del modelo AbstracUser
+# y se tienen campos por default, si quieren agregar otros campos se hace como la parte de abahi
+# class User(AbstractUser):
+# 	pass
+
+
+# se agregan nuevos campos adicionales a la herencia que tiene
 class User(AbstractUser):
-	pass
+	email = models.EmailField(unique=True)
+	pagina_web = models.CharField(max_length=255, blank=True)
+	facebook = models.CharField(max_length=255, blank=True)
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = []
